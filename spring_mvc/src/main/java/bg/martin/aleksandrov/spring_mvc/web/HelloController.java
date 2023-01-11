531cfb87e7d26b73fb0d53ecd47d075384cc6c00
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2023-01-11 10:36:51 +0100

    MVC

---
+++ spring_mvc/src/main/java/bg/martin/aleksandrov/spring_mvc/web/HelloController.java
@@ -0,0 +1,13 @@
+package bg.martin.aleksandrov.spring_mvc.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "HelloWorld";
+    }
+}
