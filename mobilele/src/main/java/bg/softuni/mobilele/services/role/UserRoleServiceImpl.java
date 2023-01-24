28630e785ad536ef158a8969a4be90c58941422f
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2023-01-24 09:18:38 +0100

    Created method getAll()

--- mobilele/src/main/java/bg/softuni/mobilele/services/role/UserRoleServiceImpl.java
+++ mobilele/src/main/java/bg/softuni/mobilele/services/role/UserRoleServiceImpl.java
@@ -1,22 +1,28 @@
 package bg.softuni.mobilele.services.role;
 
+import bg.softuni.mobilele.domain.dtos.model.UserRoleDto;
+import bg.softuni.mobilele.domain.dtos.view.UserRoleViewDto;
 import bg.softuni.mobilele.domain.entities.UserRole;
 import bg.softuni.mobilele.domain.enums.RoleEnum;
 import bg.softuni.mobilele.repositories.UserRoleRepository;
 import bg.softuni.mobilele.services.init.DataBaseInitService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserRoleServiceImpl implements UserRoleService, DataBaseInitService {
 
     private final UserRoleRepository roleRepository;
+    private final ModelMapper modelMapper;
     @Autowired
-    public UserRoleServiceImpl(UserRoleRepository roleRepository) {
+    public UserRoleServiceImpl(UserRoleRepository roleRepository, ModelMapper modelMapper) {
         this.roleRepository = roleRepository;
+        this.modelMapper = modelMapper;
     }
 
     @Override
@@ -34,5 +40,10 @@ public class UserRoleServiceImpl implements UserRoleService, DataBaseInitService
         return this.roleRepository.count() > 0;
     }
 
-//    public List<UserRoleViewModel>
+    public List<UserRoleViewDto> getAll(){
+        return this.roleRepository.findAll()
+                .stream()
+                .map(role->this.modelMapper.map(role, UserRoleViewDto.class))
+                .collect(Collectors.toList());
+    }
 }
