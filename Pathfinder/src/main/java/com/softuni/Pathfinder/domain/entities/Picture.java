099fb714a445f4cbd9175e827e6b850d00787b8c
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2023-02-06 11:52:18 +0100

    Updated Picture class

--- Pathfinder/src/main/java/com/softuni/Pathfinder/domain/entities/Picture.java
+++ Pathfinder/src/main/java/com/softuni/Pathfinder/domain/entities/Picture.java
@@ -1,11 +1,59 @@
 package com.softuni.Pathfinder.domain.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import org.hibernate.mapping.ToOne;
 
 @Entity
 @Table(name = "pictures")
 public class Picture extends BaseEntity{
 
+    @Column
+    private String title;
+    @Column(columnDefinition = "TEXT")
+    private String url;
 
+    @ManyToOne
+    private User author;
+
+    @ManyToOne
+    private Route route;
+
+    public Picture() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Picture setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Picture setUrl(String url) {
+        this.url = url;
+        return this;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public Picture setAuthor(User author) {
+        this.author = author;
+        return this;
+    }
+
+    public Route getRoute() {
+        return route;
+    }
+
+    public Picture setRoute(Route route) {
+        this.route = route;
+        return this;
+    }
 }
