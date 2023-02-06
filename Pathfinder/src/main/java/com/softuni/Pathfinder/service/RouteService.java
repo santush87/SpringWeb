48803a3f407bfb6ca915a21349fb16be1f9cf5a1
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2023-02-06 15:39:55 +0100

    Method for most commented route added

--- Pathfinder/src/main/java/com/softuni/Pathfinder/service/RouteService.java
+++ Pathfinder/src/main/java/com/softuni/Pathfinder/service/RouteService.java
@@ -1,7 +1,10 @@
 package com.softuni.Pathfinder.service;
 
+import com.softuni.Pathfinder.domain.dtos.view.MostCommentedRouteViewDto;
 import com.softuni.Pathfinder.repository.RouteRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
 
 @Service
 public class RouteService {
@@ -11,5 +14,10 @@ public class RouteService {
         this.routeRepository = routeRepository;
     }
 
-
+    public MostCommentedRouteViewDto getMostCommented() {
+        return MostCommentedRouteViewDto.fromRoute(this.routeRepository
+                .findMostCommented()
+                .orElseThrow(NoSuchElementException::new)
+                .get(0));
+    }
 }
