92ebbd5ffca602d6e6bb0e09b5a7ad6eceb673be
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2023-02-06 11:31:47 +0100

    Created User class

---
+++ Pathfinder/src/main/java/com/softuni/Pathfinder/domain/entities/User.java
@@ -0,0 +1,70 @@
+package com.softuni.Pathfinder.domain.entities;
+
+import com.softuni.Pathfinder.domain.enums.Level;
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String email;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> role;
+    @Enumerated(EnumType.STRING)
+    private Level level;
+
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public User setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public Set<Role> getRole() {
+        return role;
+    }
+
+    public User setRole(Set<Role> role) {
+        this.role = role;
+        return this;
+    }
+
+    public Level getLevel() {
+        return level;
+    }
+
+    public User setLevel(Level level) {
+        this.level = level;
+        return this;
+    }
+}
