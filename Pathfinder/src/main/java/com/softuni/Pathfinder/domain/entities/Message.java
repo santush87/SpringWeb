8eabe5357f5fbb68e9cdc0036cd3901e54985d75
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2023-02-06 12:05:49 +0100

    Created Message class

---
+++ Pathfinder/src/main/java/com/softuni/Pathfinder/domain/entities/Message.java
@@ -0,0 +1,64 @@
+package com.softuni.Pathfinder.domain.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "messages")
+public class Message extends BaseEntity{
+
+    @Column
+    private LocalDateTime dateTime;
+
+    @Column(columnDefinition = "TEXT")
+    private String textContent;
+
+    @ManyToOne
+    private User author;
+
+    @ManyToOne
+    private User recipient;
+
+    public Message() {
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public Message setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+        return this;
+    }
+
+    public String getTextContent() {
+        return textContent;
+    }
+
+    public Message setTextContent(String textContent) {
+        this.textContent = textContent;
+        return this;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public Message setAuthor(User author) {
+        this.author = author;
+        return this;
+    }
+
+    public User getRecipient() {
+        return recipient;
+    }
+
+    public Message setRecipient(User recipient) {
+        this.recipient = recipient;
+        return this;
+    }
+}
