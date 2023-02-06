98486543b80827f9b714d3e8c2665d6650e0e472
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2023-02-06 14:14:58 +0100

    Created BaseController

---
+++ Pathfinder/src/main/java/com/softuni/Pathfinder/web/BaseController.java
@@ -0,0 +1,21 @@
+package com.softuni.Pathfinder.web;
+
+import org.springframework.web.servlet.ModelAndView;
+
+public abstract class BaseController {
+
+    private static final String REDIRECT = "redirect:";
+
+    public ModelAndView view(String name, ModelAndView modelAndView){
+        modelAndView.setViewName(name);
+
+        return modelAndView;
+    }
+    public ModelAndView view(String name){
+        return this.view(name, new ModelAndView());
+    }
+
+    public ModelAndView redirect(String url){
+        return this.view(REDIRECT + url);
+    }
+}
---
+++ Pathfinder/src/main/java/com/softuni/Pathfinder/web/HomeController.java
@@ -0,0 +1,7 @@
+package com.softuni.Pathfinder.web;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+}
