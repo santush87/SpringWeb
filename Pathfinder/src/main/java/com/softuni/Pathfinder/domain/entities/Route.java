ae10894fc3aa1b45f6503fedff4e028c84fcbae6
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2023-02-06 11:57:00 +0100

    Created Route class

--- Pathfinder/src/main/java/com/softuni/Pathfinder/domain/entities/Route.java
+++ Pathfinder/src/main/java/com/softuni/Pathfinder/domain/entities/Route.java
@@ -1,9 +1,27 @@
 package com.softuni.Pathfinder.domain.entities;
 
+import com.softuni.Pathfinder.domain.enums.Level;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "routes")
 public class Route extends BaseEntity{
+
+    @Column(columnDefinition = "TEXT")
+    private String gpxCoordinates;
+
+    @Column
+    private Level level;
+
+    @Column
+    private String name;
+
+    @ManyToOne
+    private User author;
+
+    @Column
+    private String videoUrl;
 }
