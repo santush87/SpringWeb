569dcd6d26725a6c96df76ed9df502625c1aad8d
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2023-02-07 13:25:44 +0100

    Created PostMapping register

--- mobilele/src/main/java/bg/softuni/mobilele/demo/StateController.java
+++ mobilele/src/main/java/bg/softuni/mobilele/demo/StateController.java
@@ -2,21 +2,36 @@ package bg.softuni.mobilele.demo;
 
 import bg.softuni.mobilele.web.BaseController;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.net.http.HttpResponse;
 
 @Controller
 @RequestMapping("/demo")
 public class StateController extends BaseController {
 
+    private static final String STATE_USERNAME_KEY = "username";
+
     @GetMapping("/register")
-    public ModelAndView getRegister(){
+    public ModelAndView getRegister() {
         return super.view("demo/register");
     }
 
+    @PostMapping("/register")
+    public ModelAndView postRegister(HttpServletResponse response,
+                                     @RequestParam(name = STATE_USERNAME_KEY) String username) {
+        final Cookie cookie = new Cookie(STATE_USERNAME_KEY, username);
+
+        response.addCookie(cookie);
+
+        return super.redirect("login");
+    }
+
     @GetMapping("/login")
-    public ModelAndView getLogin(){
+    public ModelAndView getLogin() {
         return super.view("demo/login");
     }
 }
