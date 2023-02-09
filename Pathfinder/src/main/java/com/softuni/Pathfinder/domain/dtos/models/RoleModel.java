436388f47de9ca054a7f02e04392d4416ec1fd87
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2023-02-09 11:52:25 +0100

    Created RoleModel class

--- Pathfinder/src/main/java/com/softuni/Pathfinder/domain/dtos/models/RoleModel.java
+++ Pathfinder/src/main/java/com/softuni/Pathfinder/domain/dtos/models/RoleModel.java
@@ -1,4 +1,20 @@
 package com.softuni.Pathfinder.domain.dtos.models;
 
+import com.softuni.Pathfinder.domain.enums.RoleName;
+
 public class RoleModel {
+
+    private RoleName role;
+
+    public RoleModel() {
+    }
+
+    public RoleName getRole() {
+        return role;
+    }
+
+    public RoleModel setRole(RoleName role) {
+        this.role = role;
+        return this;
+    }
 }
