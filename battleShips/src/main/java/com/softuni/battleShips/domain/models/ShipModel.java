bf53dac3a34a7ccce0ca463d543750ea1bd73fbc
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2023-02-17 19:53:49 +0100

    Created ShipModel

---
+++ battleShips/src/main/java/com/softuni/battleShips/domain/models/ShipModel.java
@@ -0,0 +1,40 @@
+package com.softuni.battleShips.domain.models;
+
+import com.softuni.battleShips.domain.entities.Category;
+import com.softuni.battleShips.domain.entities.User;
+import com.softuni.battleShips.domain.models.CategoryModel;
+import com.softuni.battleShips.domain.models.UserModel;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class ShipModel {
+    @NotNull
+    @Size(min = 2, max = 10)
+    private String name;
+
+    @NotNull
+    @Positive
+    private Long health;
+
+    @NotNull
+    @Positive
+    private Long power;
+
+    @NotNull
+    @PastOrPresent
+    private Date created;
+
+    @NotNull
+    private CategoryModel category;
+
+}
+
