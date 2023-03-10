7312c7dbb3e1e533fa0d99ae42dae1142496bc9b
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2023-03-10 09:18:05 +0100

    Security config added loadUserByUsername

---
+++ Pathfinder/src/main/java/com/softuni/Pathfinder/service/PathfinderUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.softuni.Pathfinder.service;
+
+import com.softuni.Pathfinder.domain.entities.User;
+import com.softuni.Pathfinder.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class PathfinderUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public PathfinderUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(username).orElseThrow();
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                user.getRoles()
+                        .stream()
+                        .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getRole()))
+                        .collect(Collectors.toList())
+        );
+    }
+}
