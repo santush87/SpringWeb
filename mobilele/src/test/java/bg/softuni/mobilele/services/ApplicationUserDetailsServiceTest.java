502c7dd1e364ef5cae51cff6b87d054c5c78be8c
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2023-03-22 10:18:57 +0100

    Added testUserFound!

--- mobilele/src/test/java/bg/softuni/mobilele/services/ApplicationUserDetailsServiceTest.java
+++ mobilele/src/test/java/bg/softuni/mobilele/services/ApplicationUserDetailsServiceTest.java
@@ -1,17 +1,30 @@
 package bg.softuni.mobilele.services;
 
+import bg.softuni.mobilele.domain.entities.UserEntity;
+import bg.softuni.mobilele.domain.entities.UserRoleEntity;
+import bg.softuni.mobilele.domain.enums.RoleEnum;
 import bg.softuni.mobilele.repositories.UserRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ApplicationUserDetailsServiceTest {
+
+    private final String EXISTING_USERNAME = "Martin";
+    private final String NOT_EXISTING_USERNAME = "Pesho";
+    private final String PASSWORD = "toptop";
 
     private ApplicationUserDetailsService toTest;
 
@@ -27,13 +40,34 @@ public class ApplicationUserDetailsServiceTest {
 
     @Test
     void testUserFound() {
+
+        UserRoleEntity testAdminRole = new UserRoleEntity().setRole(RoleEnum.ADMIN);
+        UserRoleEntity testUserRole = new UserRoleEntity().setRole(RoleEnum.USER);
+
+        UserEntity testUserEntity = new UserEntity()
+                .setUsername(EXISTING_USERNAME)
+                .setPassword(PASSWORD)
+                .setRoles(List.of(testAdminRole, testUserRole));
+
+        when(mocKUserRepository.findByUsername(EXISTING_USERNAME))
+                .thenReturn(Optional.of(testUserEntity));
+
+        UserDetails adnimDetails =
+                toTest.loadUserByUsername(EXISTING_USERNAME);
+
+        //ASSERT
+        Assertions.assertNotNull(adnimDetails);
+        Assertions.assertEquals(EXISTING_USERNAME, adnimDetails.getUsername());
+        Assertions.assertEquals(testUserEntity.getPassword(), adnimDetails.getPassword());
+
+        Assertions.assertEquals(2, adnimDetails.getAuthorities().size());
     }
 
     @Test
     void testUserNotFound() {
         assertThrows(UsernameNotFoundException.class,
                 () -> {
-                    toTest.loadUserByUsername("something");
+                    toTest.loadUserByUsername(NOT_EXISTING_USERNAME);
                 });
     }
 }
