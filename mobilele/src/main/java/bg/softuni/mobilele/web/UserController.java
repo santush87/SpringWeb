f0bb072c4ad8034f7e61e5c7ec1a4b293bf58af7
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2023-09-12 11:47:37 +0300

    First commit

--- mobilele/src/main/java/bg/softuni/mobilele/web/UserController.java
+++ mobilele/src/main/java/bg/softuni/mobilele/web/UserController.java
@@ -10,10 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -21,6 +18,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/users")
+@CrossOrigin
 public class UserController extends BaseController {
     public static final String BINDING_RESULT_PATH = "org.springframework.validation.BindingResult.";
     private final UserRoleService roleService;
