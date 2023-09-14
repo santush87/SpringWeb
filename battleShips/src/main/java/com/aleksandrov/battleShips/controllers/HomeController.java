9420c6d221945f3390648af6ad4147f1f20a8faf
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2023-09-14 19:26:58 +0300

    Added HomeController

---
+++ battleShips/src/main/java/com/aleksandrov/battleShips/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package com.aleksandrov.battleShips.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String loggedOutIndex(){
+        return "index";
+    }
+    @GetMapping("/home")
+    public String loggedInIndex(){
+        return "home";
+    }
+}
